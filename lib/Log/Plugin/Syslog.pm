f033a5e91838738af8972b238b4becc9a21ada3c
masasuzu / SUZUKI Masashi <m15.suzuki.masashi@gmail.com>
2011-06-05 17:49:42 +0900

    initial commit

---
+++ lib/Log/Plugin/Syslog.pm
@@ -0,0 +1,32 @@
+package Log::Plugin::Syslog;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Log::Plugin::Syslog -
+
+=head1 SYNOPSIS
+
+  use Log::Plugin::Syslog;
+
+=head1 DESCRIPTION
+
+Log::Plugin::Syslog is
+
+=head1 AUTHOR
+
+SUZUKI Masashi / masasuzu E<lt>m15.suzuki.masahi@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
