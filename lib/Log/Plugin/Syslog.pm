ab0895498555f13c4fa768a4156f2028670a17d4
masasuzu / SUZUKI Masashi <m15.suzuki.masashi@gmail.com>
2011-06-05 19:06:52 +0900

    first commit

--- lib/Log/Plugin/Syslog.pm
+++ lib/Log/Plugin/Syslog.pm
@@ -3,6 +3,82 @@ use strict;
 use warnings;
 our $VERSION = '0.01';
 
+use Sys::Syslog;
+use Sys::Syslog qw/ :macros setlogsock /;
+
+my %_ident;
+my %_facility;
+my %_option;
+
+sub import {
+    my ($class, %args) = @_;
+    my $package = caller;
+
+    $_facility{$package} = exists $args{facility} ? $args{facility} : 'local0';
+    $_option{$package}   = exists $args{option}   ? $args{option}   : 'cons,pid';
+    $_ident{$package}    = exists $args{ident}    ? $args{ident}    : undef;
+
+    {
+        no strict 'refs'; ## no critic
+        for my $method (qw( debug info notice warning error crit emerg log) ) {
+            *{"$package\::$method"}   = \&$method;
+        }
+    }
+}
+
+
+sub debug {
+    my ($self, $message) = @_;
+    $self->log(LOG_DEBUG, $message);
+}
+
+sub info {
+    my ($self, $message) = @_;
+    $self->log(LOG_INFO, $message);
+}
+
+sub notice {
+    my ($self, $message) = @_;
+    $self->log(LOG_NOTICE, $message);
+}
+
+sub warning {
+    my ($self, $message) = @_;
+    $self->log(LOG_WARNING, $message);
+}
+
+sub error {
+    my ($self, $message) = @_;
+    $self->log(LOG_ERR, $message);
+}
+
+sub crit {
+    my ($self, $message) = @_;
+    $self->log(LOG_CRIT, $message);
+}
+
+sub alert {
+    my ($self, $message) = @_;
+    $self->log(LOG_ALERT, $message);
+}
+
+sub emerg {
+    my ($self, $message) = @_;
+    $self->log(LOG_EMERG, $message);
+}
+
+sub log {
+    my ($self, $priority, $message) = @_;
+    my $class = ref $self ? ref $self : $self;
+
+    my $ident = $_ident{$class};
+    $ident ||= $class;
+
+    openlog($ident, $_option{$class}, $_facility{$class});
+    syslog($priority, $message);
+    closelog;
+}
+
 1;
 __END__
 
@@ -12,7 +88,20 @@ Log::Plugin::Syslog -
 
 =head1 SYNOPSIS
 
-  use Log::Plugin::Syslog;
+  package Your::Module;
+  use Log::Plugin::Syslog (facility => 'local6', ident 'hoge', option => 'pid');
+
+  sub method {
+      my $self = shift;
+      $self->debug('debug');
+      $self->info('info');
+      $self->notice('notice');
+      $self->warning('warning');
+      $self->error('error');
+      $self->crit('critical');
+      $self->alert('alert');
+      $self->emerg('emergency');
+  }
 
 =head1 DESCRIPTION
 
